0c0dc368545527c2269e71c0e7327b9b6cff6a43
taihin <taihin@hotmail.com>
2017-07-30 21:38:28 +0200

    Exercise 1 descrption finished

---
+++ webdriver/WebDriverBasic/src/webdriverTraining/Ex1CreateAccountInJobbird.java
@@ -0,0 +1,28 @@
+package webdriverTraining;
+
+public class Ex1CreateAccountInJobbird {
+
+	public static void main(String[] args) {
+		
+		// Set System Property "webdriver.chrome.driver" 
+		// Set capability
+		// Create Instance of Chrome driver
+		
+		// Go to https://www.jobbird.com/
+		
+		// Click on link  Inloggen
+			
+		// Enter email addres under 'Nieuw bij Jobbird.com?'
+		
+		// Click on link 'Ga verder als nieuwe gebruiker'
+		
+		// Enter 'Gewenste wachtwoord'
+		
+		// Click on 'Account aanmaken'
+		
+		// Success message, see "Welkom `email-address`"
+
+
+	}
+
+}
