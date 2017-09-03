27ca7b52c63d747fbb4fda63d49245c49b150ed5
taihin <taihin@hotmail.com>
2017-09-03 09:03:56 +0200

    intermediate commit

---
+++ webdriver/01DemoBrowserDriverProxy/src/proxy/chrome.java
@@ -0,0 +1,22 @@
+package proxy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chrome {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.verboseLogging", "true"); // https://sites.google.com/a/chromium.org/chromedriver/logging
+
+
+	    WebDriver driver = new ChromeDriver(); // Create a new instance of the Chrome  driver
+
+		driver.get("http://www.google.nl");
+
+		Thread.sleep(6000);
+		//driver.quit();
+	}
+
+}
